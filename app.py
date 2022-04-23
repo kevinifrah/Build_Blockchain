890f4e3fe4427d1f0d56457c44b6917c70bafb83
kevinifrah <kevin.ifrah5@gmail.com>
2022-04-23 16:12:43 +0300

    add blockchain2.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-# import hashlib
+import hashlib
 
 
-# timing = datetime.now()
-# time_string = timing.strftime('%Y-%m-%d %H:%M:%S.%f')
+timing = datetime.now()
+time_string = timing.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-# encoded_block = json.dumps(block, sort_key = True).encode()
-# hash_operation = hashlib.sha256(encoded_block).hexdigest()
+encoded_block = json.dumps(block, sort_key = True).encode()
+hash_operation = hashlib.sha256(encoded_block).hexdigest()
--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,4 @@
-import datetime
-from datetime import datetime as dt
+from datetime import datetime
 import hashlib
 import json
 from flask import Flask, jsonify, request
@@ -13,8 +12,8 @@ from flask_mysqldb import MySQL
 
 class Blockchain:
 
-    def create_genesis_block():
-        the_time = dt.now()
+    def create_genesis_block(self):
+        the_time = datetime.now()
 
         block = {}
         block['index'] = 1
@@ -23,7 +22,7 @@ class Blockchain:
         block['data'] = 'This is the genesis block of the network'
         block['timestamp'] = the_time.strftime('%Y-%m-%d %H:%M:%S.%f')
 
-        encoded_block = json.dumps(block, sort_key = True).encode()
+        encoded_block = json.dumps(block, sort_keys = True).encode()
         new_hash = hashlib.sha256(encoded_block).hexdigest()
         block['new_hash'] = new_hash
 
